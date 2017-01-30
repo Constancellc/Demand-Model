c621a70d376a48caa8dbc528fb89964ccc31be6b
Constance <constancellc@gmail.com>
2017-01-30 15:33:49 +0000

    lots of ground work on new style model

--- vehicleOriented2.py
+++ vehicleOriented2.py
@@ -3,11 +3,20 @@ import numpy as np
 import csv
 import random
 
-from vehicleModel import Drivecycle, Vehicle
+from vehicleModel import Drivecycle
 
 # ------------------------------------------------------------------------------
 # CLASS DEFINITIONS SECTION
 # ------------------------------------------------------------------------------
+class Vehicle:
+    def __init__(self,mass,Ta,Tb,Tc,eff,cap):
+        self.mass = mass # kg
+        self.load = 0 #kg
+        self.Ta = Ta*4.44822 # convert lbf to N
+        self.Tb = Tb*9.9503 # lbf/mph -> N/mps
+        self.Tc = Tc*22.258 # lbf/mph2 -> N/(mps)^2 
+        self.eff = eff
+        self.capacity = cap # kWh
 
 class JourneyPool:
     def __init__(self, day, month, regionType):
@@ -69,6 +78,8 @@ class JourneyPool:
         for i in range(0,7):
             pdf[i] = purposeday[i]*purposemonth[i]*purposeregion[i]
 
+        pdf[1] = 0.5*pdf[1] # Assume all education trips are round trips
+
         sumPdf = sum(pdf)
 
         # This should be the cdf of purpose given all of day, month and region
@@ -98,7 +109,7 @@ class JourneyPool:
                     elif row[0] == (purpose+'PM'):
                         pdfPM = row[1:]
 
-                    # first get morning commmute
+                # first get morning commmute
                 ran = 0.5*random.random()
                 i = 0
                 c = float(pdfAM[0])
@@ -110,10 +121,10 @@ class JourneyPool:
 
                 ran = 0.5*random.random()
                 i = 0
-                c = pdfPM[0]
+                c = float(pdfPM[0])
                 while c <= ran:
                     i += 1
-                    c += pdfPM[i]
+                    c += float(pdfPM[i])
 
                 backStartHour = i
                 
@@ -127,14 +138,19 @@ class JourneyPool:
 
                 times = []
 
-                for i in range(0,2):
+                for j in range(0,2):
                     ran = random.random()
                     i = 0
                     c = float(pdf[0])
-                    while c <= ran:
+                    while c <= ran and i<23:
                         i += 1
                         c += float(pdf[i])
 
+                    # hack for shifting time
+                    if i > 3:
+                        i -= 4
+                    else:
+                        i += 20
                     times.append(i)
 
                 if times[0] < times[1]:
@@ -144,6 +160,8 @@ class JourneyPool:
                 else:
                     outStartHour = times[1]
                     backStartHour = times[0]
+
+
 
         with open('nts-data/regionTypePurposeLength.csv','rU') as csvfile:
             reader = csv.reader(csvfile)
@@ -156,8 +174,28 @@ class JourneyPool:
                 if row[0] == purpose:
                     # generate distance in miles
                     distance = np.random.normal(float(row[regionIndex]),1)
-        outTime = 60*outStartHour + int(60*random.random())
-        backTime = 60*backStartHour + int(60*random.random())
+
+                    # assume all education journeys are round trips
+                    if purpose == 'Education':
+                        distance = distance*2
+
+        if outStartHour == backStartHour:
+            time1 = 0
+            time2 = 0
+            while (time1-time2)*(time1-time2) <= 400:
+                time1 = outStartHour*60 + int(60*random.random())
+                time2 = outStartHour*60 + int(60*random.random())
+
+            if time1 < time2:
+                outTime = time1
+                backTime = time2
+            else:
+                outTime = time2
+                backTime = time1
+
+        else:        
+            outTime = 60*outStartHour + int(60*random.random())
+            backTime = 60*backStartHour + int(60*random.random())
 
         self.journeys[purpose].append([outTime,backTime,distance])
 
@@ -188,22 +226,207 @@ class JourneyPool:
 
         self.journeys[purpose].remove(journey)
 
-        return journey[0]
+        return [purpose,journey[0],journey[1],journey[2]]
     
 class Agent:
-    def __init__(self, vehicle, regionType):
+    def __init__(self, name, vehicle, regionType):
         # vehicle - a loaded instance of the Vehicle class, regionType - string
+        self.name = name
         self.vehicle = vehicle
         self.regionType = regionType
         self.avaliability = [1]*(24*60)
         self.location = [0]*(24*60)
-        self.journeys = []
-        
+        self.journeysLog = []
+        self.energyLog = []
+        #self.mileage = 0
+        self.battery = vehicle.capacity
+        self.energySpent = [0]*(24*60)
 
+    def addJourney(self,journey):
+        purpose = journey[0]
+        timeOut = journey[1]
+        timeBack = journey[2]
+
+        # hack to get around negative journey lengths
+        if journey[3] < 0:
+            journey[3] = -journey[3]
+        distance = journey[3]*1609.34 #convert from miles to m
+
+        self.journeysLog.append(journey)
+
+        #self.mileage += distance*2
+
+        # calculate the energy expenditure
+        if self.regionType[0] == 'U':
+            cycle = Drivecycle(distance,'urban')
+        elif self.regionType[0] == 'R':
+            cycle = Drivecycle(distance,'rural')
+        else:
+            raise Exception('region not recognised')
+
+        v = cycle.velocity # m/s
+        a = cycle.acceleration # m/s2
+
+        F = []
+        for value in v:
+            F.append(self.vehicle.Ta + self.vehicle.Tb*value +
+                     self.vehicle.Tc*value*value)
+
+        E = 0
+        for i in range(0,len(a)):
+            F[i] += (self.vehicle.mass+self.vehicle.load)*a[i]
+
+            if a[i] >= 0:
+                E += F[i]*v[i]/self.vehicle.eff
+            else:
+                E += F[i]*v[i]*self.vehicle.eff
+
+        energy = E*2.77778e-7 # kWh
+        length = int(float(len(v))/60) # minutes
+
+        # first update details for outward journey
+
+        p = 0
+        for i in range(timeOut,timeOut+length):
+            if i >= 24*60:
+                i -= 24*60
+            p += 1/float(length)
+            self.avaliability[i] = 0
+            self.location[i] = 1
+            self.energySpent[i] += energy*p
+
+        for i in range(timeOut+length,timeBack):
+            if purpose == 'Commute':
+                self.location[i] = 2
+            elif purpose == 'Education':
+                self.location[i] = 0  #PANIC, ok might be sorted
+            else:
+                self.location[i] = 3
+            self.avaliability[i] = 0
+            self.energySpent[i] += energy
+
+        p = 0
+        for i in range(timeBack,timeBack+length):
+            if i >= 24*60:
+                i -= 24*60
+            p += 1/float(length)
+            self.avaliability[i] = 0
+            self.location[i] = 1
+            self.energySpent[i] += energy*(1+p)
+
+        if timeBack+length < 24*60:
+            for i in range(timeBack+length,24*60):
+                self.energySpent[i] += 2*energy
+
+        self.energyLog.append([timeOut+length,energy])
+        self.energyLog.append([timeBack+length,energy])
+
+    def Charge(timeOn,timeOff,power):
+        # put vehicle on charge
+        for i in range(timeOn,timeOff):
+            self.energySpent[i] -= power/60
+    
+class Fleet:
+    def __init__(self):
+        self.n = 0
+        self.fleet = []
+
+    def addAgent(self,agent):
+        self.n += 1
+        self.fleet.append(agent)
+
+    def pickAvaliableAgent(self,timeA,timeB):
+        avaliableAgents = []
+        for i in range(0,self.n):
+            j = timeA
+            while self.fleet[i].avaliability[j] == 1 and j < timeB:
+                j += 1
+            if j == timeB:
+                avaliableAgents.append(self.fleet[i])
+
+        if avaliableAgents == []:
+            print timeA
+            print timeB
+            raise Exception('no avaliable agents between times ' + str(timeA) +
+                            ' and ' + str(timeB))
+        else:
+            ran = int(random.random()*len(avaliableAgents))
+            return avaliableAgents[ran]
+
+    def getFleetLocations(self):
+        home = [0]*(24*60)
+        work = [0]*(24*60)
+        driving = [0]*(24*60)
+        other = [0]*(24*60)
+        
+        for i in range(0,self.n):
+            for j in range(0,24*60):
+                if self.fleet[i].location[j] == 0:
+                    home[j] += 1
+                elif self.fleet[i].location[j] == 1:
+                    driving[j] += 1
+                elif self.fleet[i].location[j] == 2:
+                    work[j] += 1
+                elif self.fleet[i].location[j] == 3:
+                    other[j] += 1
+                else:
+                    raise Exception('location problem')
+
+        output = []
+        output.append(home)
+        output.append(work)
+        output.append(driving)
+        output.append(other)
+        
+        return output
+
+    def sortFleetEnergyLogs(self):
+
+        for i in range(0,self.n):
+            oldLog = self.fleet[i].energyLog
+            newLog = []
+
+            while len(oldLog) > 0:
+                earliest = oldLog[0]
+                for j in range(1,len(oldLog)):
+                    if oldLog[j][0] < earliest[0]:
+                        earliest = oldLog[j]
+
+                newLog.append(earliest)
+                oldLog.remove(earliest)
+
+            self.fleet[i].energyLog = newLog
+                    
+                    
+    def getFleetExpenditure(self):
+        power = [0]*(24*60)
+
+        for i in range(0,self.n):
+            for j in range(0,24*60):
+                power[j] += self.fleet[i].energySpent[j]
+
+        return power
+
+class ChargingScheme:
+
+    def __init__(self, fleet):
+        self.fleet = fleet
+
+
+
+class HomeOnly(ChargingScheme):
+
+    def allCharge(self,power):
+        for i in range(0,self.fleet.n):
+            for j in range(0,24*60):
+                # first check if vehicle is at home
+                if self.fleet.fleet[i].location[j] == 0:
+                    # next check if vehicle needs charge
+                    return ''
+    
 # ------------------------------------------------------------------------------
 # INITIALIZATION SECTION
 # ------------------------------------------------------------------------------
-
 regionType = 'Urban City and Town'
 region = ''
 month = 'May'
@@ -242,24 +465,56 @@ numberAgents = int(carsPerPerson*population)
 
 # ok, now we know how much of everything we need let's start generating
 
+nissanLeaf = Vehicle(1705,33.78,0.0618,0.02282,0.7,32)
+
+fleet = Fleet()
+for k in range(0,numberAgents):
+    agent = Agent(str(k), nissanLeaf, regionType)
+    fleet.addAgent(agent)
+
+print str(numberAgents) + ' agents were initialised'
+
 # first the pool
 pool = JourneyPool(day, month, regionType)
 
-for i in range(0,10):
+for k in range(0,numberJourneys):
     pool.addJourney()
 
+print str(numberJourneys) + ' journeys were generated'
+
+for k in range(0,numberJourneys):
+    journey = pool.pickOutJourney()
+    agent = fleet.pickAvaliableAgent(journey[1],journey[2])
+    agent.addJourney(journey)
+    #print 'A '+journey[0] + ' trip was given to agent#' + agent.name
+
+print 'All journeys assigned!'
+t = np.linspace(4,28,num=24*60)
+fleet.sortFleetEnergyLogs()
+print 'All logs sorted, here is some proof:'
+print fleet.fleet[4].energyLog
+n = fleet.getFleetLocations()
+plt.figure(1)
+plt.plot(t,n[0],label='Home')
+plt.plot(t,n[2],label='Work')
+plt.plot(t,n[3],label='Other')
+plt.plot(t,n[1],label='In Transit')
+plt.ylim((0,fleet.n+1))
+plt.xlim((4,28))
+plt.legend(loc=1)
+plt.show()
+
+totalPower = fleet.getFleetExpenditure()
+plt.figure(2)
+plt.plot(t,totalPower)
+plt.show()
+"""
+plt.figure(1)
 for i in range(0,10):
-    print pool.pickOutJourney()
+    plt.subplot(5,2,i+1)
+    plt.plot(t,fleet.fleet[i].location,t,fleet.fleet[i].avaliability)
+plt.show()
+"""
 
-
-fleet = {}
-nissanLeaf = Vehicle(1705,33.78,0.0618,0.02282,0.7)
-nissanLeaf.loadEnergies()
-
-print numberAgents
-print numberJourneys
-
-for i in range(0,numberAgents):
-    fleet['vehicle'+str(i)] = Agent(nissanLeaf,regionType)
 #plt.plot(np.linspace(0,24,num=24*60),pool.commute)
 #plt.show()
