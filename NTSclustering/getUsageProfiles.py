584d35fb8c9c6d27f67276499628325db21aee68
Constance <constancellc@gmail.com>
2017-06-13 23:44:13 +0100

    no message

--- NTSclustering/getUsageProfiles.py
+++ NTSclustering/getUsageProfiles.py
@@ -63,16 +63,26 @@ with open(data,'rU') as csvfile:
                 index += 1
                 if index >= 48:
                     index -= 48
-wTotal = [0]*48
-weTotal = [0]*48
 
-for vehicle in wProfiles:
-    for i in range(0,48):
-        wTotal[i] += wProfiles[vehicle][i]
 
-for vehicle in weProfiles:
-    for i in range(0,48):
-        weTotal[i] += weProfiles[vehicle][i]
+def polyfit(series,dof,plot=False,clr='b'):
+    p_ = np.polyfit(range(0,len(series)),series,dof)
+
+    def p(x):
+        rv = 0
+        for i in range(0,len(p_)):
+            rv += p_[len(p_)-1-i]*x**i
+        return rv
+
+    y = []
+    for i in np.arange(0,len(series),0.1):
+        y.append(p(i))
+
+    if plot == True:
+        plt.plot(np.arange(0,len(series),0.1),y,clr)
+
+    return p_
+
 
 x = np.arange(8,48,8)
 x_ticks = range(4,24,4)
@@ -81,16 +91,44 @@ for i in range(0,len(x_ticks)):
         x_ticks[i] = '0'+str(x_ticks[i])+':00'
     else:
         x_ticks[i] = str(x_ticks[i])+':00'
+        
+wTotal = [0]*48
+weTotal = [0]*48
+
+clrs = ['k','r','b','g','y']
 
 plt.figure(1)
 plt.subplot(2,1,1)
+n = 0
+for vehicle in wProfiles:
+    if n < 5:
+            polyfit(wProfiles[vehicle],20,plot=True,clr=clrs[n])
+            plt.plot(range(0,48),wProfiles[vehicle],clrs[n]+'x')
+            n += 1
+    for i in range(0,48):
+        wTotal[i] += wProfiles[vehicle][i]
+
+plt.subplot(2,1,2)
+n = 0
+for vehicle in weProfiles:
+    if n < 5:
+            polyfit(weProfiles[vehicle],20,plot=True,clr=clrs[n])
+            plt.plot(range(0,48),weProfiles[vehicle],clrs[n]+'x')
+            n += 1
+    for i in range(0,48):
+        weTotal[i] += weProfiles[vehicle][i]
+
+plt.figure(2)
+plt.subplot(2,1,1)
 plt.bar(range(0,48),wTotal)
+polyfit(wTotal,20,plot=True)
 plt.title('Week Day')
 plt.xticks(x,x_ticks)
 plt.xlim(0,48)
 
 plt.subplot(2,1,2)
 plt.bar(range(0,48),weTotal)
+polyfit(weTotal,20,plot=True)
 plt.title('Weekend')
 plt.xticks(x,x_ticks)
 plt.xlim(0,48)
