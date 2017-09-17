1d07c14e6b71ea5f6c48d3baf8cb15213852f77a
Constance Crozier <Constance@Constances-MacBook-Pro-3.local>
2017-09-17 18:14:14 +0100

    no message

--- US-NHTS/test.py
+++ US-NHTS/test.py
@@ -5,36 +5,24 @@ import numpy as np
 # my code
 from NHTSenergyPrediction import NationalEnergyPrediction, EnergyPrediction, BaseLoad
 
-'''
-test = EnergyPrediction('3','4',car='tesla')
-base = BaseLoad('3','3',36)
-profiles = test.getOptimalChargingProfiles(22,base.baseLoad)
-plt.figure(1)
-summed = [0]*36
-n = 0
-for v in profiles:
-    n += 1
-    for i in range(0,36):
-        summed[i] += profiles[v][i]
-plt.plot(summed)
-plt.plot(np.linspace(0,36,num=36*60), base.baseLoad)
-print(n)
-plt.show()
-'''
-plotN = {'1':1,'4':2,'7':3,'10':4}
-plotTitle = {'1':'Jan','4':'Apr','7':'Jul','10':'Oct'}
+day = '3'
 
-x_ticks = ['08:00','12:00','16:00','20:00','00:00','04:00','08:00']
-x = np.arange(8,36,4)
+plotN = {'1':1,'4':2,'7':3,'10':4}
+plotTitle = {'1':'January','4':'April','7':'July','10':'October'}
+
+
+x = np.linspace(8,32,num=5)
+x_ticks = ['08:00 \n Wed','14:00','20:00','02:00','08:00 \n Thu']
 t = np.linspace(0,36,num=36*60)
 plt.figure(1)
+
+plt.rcParams["font.family"] = 'serif'
 for month in ['1','4','7','10']:
     plt.subplot(2,2,plotN[month])
     plt.title(plotTitle[month],y=0.85)
-    base = BaseLoad('3',month,36,unit='k').getLoad()
-    test = NationalEnergyPrediction('3',month,vehicle='teslaS60D',smoothTimes=True)
-    print(test.getNumberOfVehicles(),end=' ')
-    print('vehicles')
+    base = BaseLoad(day,month,36,unit='k').getLoad()
+    test = NationalEnergyPrediction(day,month,vehicle='teslaS60D',smoothTimes=True)
+
     dumb = test.getDumbChargingProfile(3.5,36,extraCharge=False)
     totalEn = 0
     for i in range(0,36*60):
@@ -51,14 +39,17 @@ for month in ['1','4','7','10']:
             opt[int(i/60)] += base[i]
             opt[int(i/60)] = opt[int(i/60)]/1000000
         base[i] = base[i]/1000000
-    plt.plot(t,dumb)
-    plt.plot(t,base,'g',ls=':')
-    plt.plot(opt)
+    plt.plot(t,dumb,label='Uncontrolled Charging')
+    plt.plot(t,base,'g',ls=':',label='Base Load')
+    plt.plot(opt,ls='--',label='Controlled Charging')
     plt.xticks(x,x_ticks)
     plt.xlim(7,33)
     plt.ylim(300,900)
     plt.xlabel('Time')
     plt.ylabel('Power (GW)')
     plt.grid()
+    
+    if month == '1':
+        plt.legend(loc=[-0.2,1.1],ncol=3)
 plt.show()
 
