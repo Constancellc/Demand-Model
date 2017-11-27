85e485540f3a5f404349ed089d5be6fe63876ef8
Constance <constancellc@gmail.com>
2017-11-27 16:23:20 +0000

    no message

--- NGclustering/clusterDemand.py
+++ NGclustering/clusterDemand.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import sklearn.cluster as clst
+import datetime
 
 maxDemand = 0
 
@@ -42,6 +43,75 @@ for date in profiles:
         data.append(profiles[date])
         dates.append(date)
 
+centroid, label, inertia = clst.k_means(data,8)
+
+clusteredDates = {}
+
+for i in range(0,len(label)):
+    c = label[i]
+
+    if c not in clusteredDates:
+        clusteredDates[c] = []
+
+    d = datetime.datetime(int(dates[i][1:5]),int(dates[i][6:8]),
+                          int(dates[i][9:11]))
+
+    clusteredDates[c].append([dates[i][1:],d.isoweekday(),d.month])
+
+days = ['mon','tue','wed','thu','fri','sat','sun']
+ms = ['jan','feb','mar','apr','may','jun',' jul','aug','sep','oct',
+      'nov','dec']
+
+plt.figure(1)
+
+totalN = len(label)
+
+os = {0:-0.4,1:-0.3,2:-0.2,3:-0.1,4:0,5:0.1,6:0.2,7:0.3}
+for c in clusteredDates:
+    wd = [0]*7
+    m = [0]*12
+
+    for d in clusteredDates[c]:
+        wd[d[1]-1] += 1
+        m[d[2]-1] += 1
+        
+    n = len(clusteredDates[c])
+    '''
+
+    print('----------')
+    print('cluster '+str(c))
+    print('----------')
+    print('')
+    for i in range(0,len(wd)):
+        print(days[i]+str(int(100*wd[i]/n))+'% ',end=' ')
+    print('')
+    print('months')
+    for i in range(0,len(m)):
+        print(ms[i]+str(int(100*m[i]/n))+'% ',end='')
+    print('')
+    '''
+
+    for i in range(0,len(wd)):
+        wd[i] = wd[i]/n
+    for i in range(0,len(m)):
+        m[i] = m[i]/n
+
+    plt.subplot(2,4,c+1)
+    plt.pie(wd,center=[0.5,0.5],radius=0.55,labels=days)
+    plt.pie(m,center=[1.9,0.5],radius=0.55,labels=ms)
+    plt.title('cluster '+str(c)+': '+str(int(100*len(clusteredDates[c])/totalN))+'% points',
+              x=0.2)
+
+plt.figure(2)
+for i in range(8):
+    plt.plot(centroid[i],label=str(i))
+plt.legend()
+
+
+
+plt.show()
+
+'''
 k_values = [6,8,10,12,14,16]
 
 plt.figure(1)
@@ -54,3 +124,4 @@ for j in range(0,6):
     plt.title('k='+str(k_values[j]))
     
 plt.show()
+'''
