2144dadd81a4b96ffc2863e544dd07112428eaeb
Constance Crozier <Constance@Constances-MacBook-Pro-3.local>
2018-01-20 17:35:58 +0000

    no message

--- variability/viewLfLmLossesResults.py
+++ variability/viewLfLmLossesResults.py
@@ -18,8 +18,8 @@ with open('results/lf_lm_losses.csv','rU') as csvfile:
             continue
         for i in range(3):
             totals[i].append(100*float(row[i])/float(row[3]))
-        diffs[0].append(float(row[4]))
-        diffs[1].append(float(row[5]))
+        diffs[0].append(100*float(row[4])/float(row[3]))
+        diffs[1].append(100*float(row[5])/float(row[3]))
 
 av = []
 for key in range(3):
@@ -31,19 +31,19 @@ x2_ticks = ['Load\nFlattening','Loss\nMinimizing','Uncontrolled']
 plt.figure(1)
 #plt.subplot(2,1,1)
 plt.rcParams["font.family"] = 'serif'
-plt.rcParams['font.size'] = 8
+plt.rcParams['font.size'] = 10
 plt.boxplot([totals[0],totals[1],totals[2]],0,'',whis=[0.05, 99.5])
 plt.xticks([1,2,3],x2_ticks)
-plt.plot([0.5,3.5],[av[1],av[1]])
+plt.plot([0.5,3.5],[av[1],av[1]],'k',ls=':',alpha=0.5)
 plt.grid()
 plt.ylabel('Losses (%)')
-'''
-plt.subplot(2,1,2)
+
+plt.figure(4)
 plt.scatter(diffs[0],diffs[1])
 #plt.plot([0,],[0,])
 plt.ylabel('predicted')
 plt.xlabel('observed')
-'''
+
 loads = {'lf':[],'lm':[],'uc':[]}
 for typ in ['lf','lm','uc']:
     with open('results/'+typ+'_loads.csv','rU') as csvfile:
@@ -78,7 +78,7 @@ plt.subplot(1,2,2)
 for typ in ['lf','lm','uc']:
     av = [0.0]*1440
     for t in range(1440):
-        av[t] = sum(loads[typ][t])/len(loads[typ])
+        av[t] = sum(loads[typ][t])/(55*len(loads[typ][t]))
     
     plt.plot(av,clrs[typ])
 
@@ -86,22 +86,30 @@ plt.figure(3)
 hh = [0,54]
 for i in range(2):
     plt.subplot(2,1,i+1)
-    lm = []
-    lf = []
-    bl = []
+    lm = [0.0]*1440
+    lf = [0.0]*1440
+    bl = [0.0]*1440
     with open('results/lf_lm_inidividuals'+str(hh[i])+'.csv','rU') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)
+        r = 0
         for row in reader:
             if row == []:
                 continue
-            lf.append(float(row[1]))
-            lm.append(float(row[2]))
-            bl.append(float(row[3]))
-        plt.plot(lf)
-        plt.plot(lm)
-        plt.plot(bl)
+            for c in range(100):
+                lf[r] += float(row[3*c+1])/100
+                lm[r] += float(row[3*c+2])/100
+                bl[r] += float(row[3*c+3])/100
+            r += 1
+    plt.plot(lf,label='load flattening')
+    plt.plot(lm,label='loss minimising')
+    plt.plot(bl,'k',ls=':',alpha=0.5,label='base load')
+    plt.grid()
+    plt.ylim(0,1)
+    plt.ylabel('Power (kW)')
+    plt.title('Household '+str(hh[i]+1))
+    plt.xlim(0,1440)
 
-plt.show()
+plt.legend(loc=[0.2,2.3],ncol=3)
 plt.show()
             
