e0dfcf63c834ae228f538e7ef59b6e1c66ab2286
Constance <constancellc@gmail.com>
2018-02-22 15:47:44 +0000

    no message

--- HH-prediction/GP.py
+++ HH-prediction/GP.py
@@ -11,15 +11,20 @@ I definitely want to make a covariance matrix class
 
 '''
 
+def se(a,b,h,l):
+    return h*h*np.exp(-np.power(a-b,2)/(2*l*l))
+def p(a,b,h,l,omega):
+    return h*h*np.exp(-2*np.power(np.sin((omega*(a-b))/2)/l,2))
+
 class CovMatrix:
     def __init__(self,x1,x2,theta0):
         # storing the hyper-parameters
         self.h1 = theta0[0]
         self.l1 = theta0[1]
         self.h2 = theta0[2]
-        self.h3 = theta0[3]
-        self.l2 = 48 # 1 day
-        self.l3 = 336 # 1 week
+        self.l2 = theta0[3]
+        self.h3 = theta0[4]
+        self.l3 = theta0[5] 
 
         # storing the input vectors
         self.x1 = x1
@@ -41,17 +46,14 @@ class CovMatrix:
         self.K2 = np.matrix(np.zeros((self.n,self.m)))
         self.K3 = np.matrix(np.zeros((self.n,self.m)))
 
-        def se(a,b,h,l):
-            return h*h*np.exp(-np.power(a-b,2)/(2*l*l))
-        def p(a,b,h,l):
-            return h*h*np.exp(-2*np.power(np.sin((a-b)/2)/l,2))
-
         if self.square is True:
             for i in range(self.n):
                 for j in range(self.n):
                     self.K1[i,j] = se(self.x1[0,i],self.x2[0,j],self.h1,self.l1)
-                    self.K2[i,j] = p(self.x1[0,i],self.x2[0,j],self.h2,self.l2)
-                    self.K3[i,j] = p(self.x1[0,i],self.x2[0,j],self.h3,self.l3)
+                    self.K2[i,j] = p(self.x1[0,i],self.x2[0,j],self.h2,self.l2,
+                                     2*np.pi/48) # one day
+                    self.K3[i,j] = p(self.x1[0,i],self.x2[0,j],self.h3,self.l3,
+                                     2*np.pi/336) # one week
 
                     self.K1[j,i] = self.K1[i,j]
                     self.K2[j,i] = self.K2[i,j]
@@ -60,8 +62,10 @@ class CovMatrix:
             for i in range(self.n):
                 for j in range(self.m):
                     self.K1[i,j] = se(self.x1[0,i],self.x2[0,j],self.h1,self.l1)
-                    self.K2[i,j] = p(self.x1[0,i],self.x2[0,j],self.h2,self.l2)
-                    self.K3[i,j] = p(self.x1[0,i],self.x2[0,j],self.h3,self.l3)
+                    self.K2[i,j] = p(self.x1[0,i],self.x2[0,j],self.h2,self.l2,
+                                     2*np.pi/48) # one day)
+                    self.K3[i,j] = p(self.x1[0,i],self.x2[0,j],self.h3,self.l3,
+                                     2*np.pi/336) # one week
 
         
         self.K = self.K1+self.K2+self.K3
@@ -69,12 +73,22 @@ class CovMatrix:
         self.foundInv = False
         self.foundChol = False
 
+    def add_noise(self,sigma):
+
+        if self.square == False:
+            return ''
+
+        else:
+            self.K += sigma*np.eye(self.n)
+
     def update_hyperparameters(self,theta1):
 
         self.h1 = theta1[0]
         self.l1 = theta1[1]
         self.h2 = theta1[2]
-        self.h3 = theta1[3]
+        self.l2 = theta1[3]
+        self.h3 = theta1[4]
+        self.l3 = theta1[5] 
 
         self.calc_matrix()       
 
@@ -82,16 +96,11 @@ class CovMatrix:
         # check square
         if self.square == False:
             raise Exception()
-
-        if sigma > 0:
-            self.K += sigma*np.eye(self.n)
         
         if len(b) == 0:
             if self.foundInv == False:
                 self.iK = np.linalg.inv(self.K)
                 self.foundInv = True
-            if sigma > 0:
-                self.K -= sigma*np.eye(self.n)
             return self.iK
             
         else:
@@ -107,8 +116,7 @@ class CovMatrix:
                 self.iL = np.linalg.inv(self.L)
                 self.iU = np.linalg.inv(self.L.H)
                 self.foundChol = True
-            if sigma > 0:
-                self.K -= sigma*np.eye(self.n)
+                
             if len(b) == 1:
                 return self.iU*self.iL*b.T
             else:
@@ -120,47 +128,67 @@ class CovMatrix:
     def set_training_pts(self,y):
         self.y = y
 
+        d = []
+        mean = 0
+        for i in range(y.size):
+            mean += y[0,i]/y.size
+        for i in range(y.size):
+            d.append(y[0,i]-mean)
+        self.d = np.matrix(d).T
+
     def f(self,theta):
-        mean = theta[0]
-        d = self.y-np.matrix([mean]*self.y.size)
-        d = d.T
 
-        self.update_hyperparameters(theta[1:])
+        self.update_hyperparameters(theta)
+        self.add_noise(1e-3)
 
-        f = np.log(self.det()) + d.T*self.inv(d)
+        f = np.log(self.det()) + self.d.T*self.inv(self.d)
 
-        #print(f)
         return f
 
     def g(self,theta):
-        mean = theta[0]
-        d = self.y-np.matrix([mean]*self.y.size)
-        d = d.T
         
-        self.update_hyperparameters(theta[1:])
+        self.update_hyperparameters(theta)
+        self.add_noise(1e-3)
 
         # check k(x,x)
         if self.x1.all != self.x2.all:
             raise Exception()
         
-        g = [0.0]*5
+        g = [0.0]*6
 
         dKdh1 = 2*self.K1/self.h1
         dKdh2 = 2*self.K2/self.h2
         dKdh3 = 2*self.K3/self.h3
         
         dKdl1 = np.matrix(np.zeros((self.n,self.m)))
+        dKdl2 = np.matrix(np.zeros((self.n,self.m)))
+        dKdl3 = np.matrix(np.zeros((self.n,self.m)))
         for i in range(self.n):
             for j in range(self.m):
-                dKdl1[i,j] = np.power(self.x1[0,i]-self.x2[0,j],2)*\
-                             self.K1[i,j]/np.power(self.l1,3)
+                dij = self.x1[0,i]-self.x2[0,j]
+                dKdl1[i,j] = np.power(dij,2)*self.K1[i,j]/np.power(self.l1,3)
                 dKdl1[j,i] = dKdl1[i,j]
+                
+                dKdl2[i,j] = np.power(2*np.sin(np.pi*dij/48),2)*self.K2[i,j]\
+                             /np.power(self.l2,3)
+                dKdl2[j,i] = dKdl2[i,j]
 
-        g[0] = -2*self.inv(d)[0,0]
-        g[1] = (np.trace(self.inv(dKdh1))-d.T*self.inv(dKdh1)*self.inv(d))[0,0]
-        g[2] = (np.trace(self.inv(dKdl1))-d.T*self.inv(dKdl1)*self.inv(d))[0,0]
-        g[3] = (np.trace(self.inv(dKdh2))-d.T*self.inv(dKdh2)*self.inv(d))[0,0]
-        g[4] = (np.trace(self.inv(dKdh3))-d.T*self.inv(dKdh3)*self.inv(d))[0,0]
+                dKdl3[i,j] = np.power(2*np.sin(np.pi*(dij)/336),2)*self.K3[i,j]\
+                             /np.power(self.l3,3)
+                dKdl3[j,i] = dKdl3[i,j]
+        
+        g[0] = (np.trace(self.inv(dKdh1))-\
+                self.d.T*self.inv(dKdh1)*self.inv(self.d))[0,0]
+        g[1] = (np.trace(self.inv(dKdl1))-\
+                self.d.T*self.inv(dKdl1)*self.inv(self.d))[0,0]
+        g[2] = (np.trace(self.inv(dKdh2))-\
+                self.d.T*self.inv(dKdh2)*self.inv(self.d))[0,0]
+        g[3] = (np.trace(self.inv(dKdl2))-\
+                self.d.T*self.inv(dKdl2)*self.inv(self.d))[0,0]
+        g[4] = (np.trace(self.inv(dKdh3))-\
+                self.d.T*self.inv(dKdh3)*self.inv(self.d))[0,0]
+        g[5] = (np.trace(self.inv(dKdl3))-\
+                self.d.T*self.inv(dKdl3)*self.inv(self.d))[0,0]
 
         #print(g)                       
         return g
@@ -179,39 +207,48 @@ class CovMatrix:
                 
 class GaussianProcess:
     def __init__(self):
+        self.mean = 0.0
         self.theta = None
         self.x = None
         self.y = None
 
-    def learn_hyperparameters(self,x,y,theta0=['',1,2,1,1]):
+    def learn_hyperparameters(self,x,y,theta0=[0.18,0.08,0.17,1.2,0.15,0.05]):
         # check length of x and y the same
 
         self.x = x
         self.y = y
 
-        # set inital mean estimate to the average
-        if theta0[0] == '':
-            n = x.size
-            theta0[0] = 0
-            for i in range(n):
-                theta0[0] += y[0,i]/n
+        # set mean to average observed value
+        self.mean = 0
+        for i in range(y.size):
+            self.mean += y[0,i]/y.size
+
         self.cov = CovMatrix(x,x,theta0)
+        self.cov.add_noise(1e-3)
         self.cov.set_training_pts(y)
-        # manually adding noise
-        #self.cov.K = self.cov.K + 1e-3*np.eye(self.cov.n)
-        self.cov.check_derivatives(theta0)
+
+        #self.cov.check_derivatives(theta0)
         self.theta = opt.fmin_tnc(self.cov.f,theta0,fprime=self.cov.g,
-                                  bounds=[[0.01,10]]*5)[0]
-        #self.theta = opt.fmin_tnc(self.cov.f,theta0,
-        #                          bounds=[[0.01,5]]*5)[0]
+                                  bounds=[[0.01,100]]*6)[0]
+        #self.theta = opt.minimize(self.cov.f,theta0,bounds=[[0.01,100]]*6)['x']
+
         print(self.theta)
-        self.cov.update_hyperparameters(self.theta[1:])
+        self.cov.update_hyperparameters(self.theta)
+        self.cov.add_noise(1e-3)
 
     def train(self,x,y,theta0=None):
         self.x = x
         self.y = y
+        
+        # set mean to average observed value
+        self.mean = 0
+        for i in range(y.size):
+            self.mean += y[0,i]/y.size
+
         if theta0 != None:
             self.theta = theta0
+
+        print(self.theta)
 
         if self.theta is None:
             print('need to learn hyperparameters first')
@@ -222,10 +259,11 @@ class GaussianProcess:
         m = np.matrix([self.theta[0]]*x_.size).T
         d = self.y-np.matrix([self.theta[0]]*self.y.size)
         K2 = CovMatrix(x_,self.x,self.theta)
-        m += K2.K*self.cov.inv(d,sigma=1e-3)
+        m += K2.K*self.cov.inv(d)
 
-        cov = CovMatrix(x_,x_,self.theta).K-K2.K*self.cov.inv(K2.K.T,sigma=1e-3)
-        var = np.diag(cov)
-        print(var)
+        #cov = CovMatrix(x_,x_,self.theta).K-K2.K*self.cov.inv(K2.K.T,sigma=1e-3)
+        #var = np.diag(cov)
+        #print(var)
+        var = None
         m = np.squeeze(np.asarray(m))
         return [m,var]
--- HH-prediction/test.py
+++ HH-prediction/test.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import copy
 import csv
-
+import random
 #'''
 
 x = []
@@ -13,26 +13,61 @@ with open('../../Documents/sharonb/7591/csv/5110.csv','rU') as csvfile:
     for row in reader:
         x.append(float(row[0]))
         y.append(float(row[1]))
-        
-x_train = np.matrix(x[:50])
-y_train = np.matrix(y[:50])
+
+'''
+# using 50 random training pts from first 500 hrs
+chosen = []
+while len(chosen) < 300:
+    ran = int(random.random()*700)
+    if ran not in chosen:
+        chosen.append(ran)
+x_train = []
+y_train = []
+chosen = sorted(chosen)
+for i in range(300):
+    x_train.append(x[chosen[i]])
+    y_train.append(y[chosen[i]])
+
+x_train = np.matrix(x_train)
+y_train = np.matrix(y_train)
+'''
+x_train = np.matrix(x[:700])
+y_train = np.matrix(y[:700])
+x_test = x[700:850]
+y_test = y[700:850]
 #'''
 GP = GaussianProcess()
-#GP.train(np.matrix(range(10)),np.matrix([0.2,0.3]*5),[0.3,1.3,4,0.5,0.2])
-GP.learn_hyperparameters(x_train,y_train)
-test = np.arange(1,10,0.1)
+theta = [0.13456641,0.08,0.16965769,1.30353067,0.145682,0.03243102]
+GP.train(x_train,y_train,theta)
+#GP.learn_hyperparameters(x_train,y_train)
+test = np.arange(1,850,1)
 [m,var] = GP.predict(np.matrix(test))
-
+'''
 u = copy.copy(m)
 l = copy.copy(m)
 for i in range(len(m)):
     u[i] += 2*np.sqrt(var[i])
     l[i] -= 2*np.sqrt(var[i])
-    
+'''
+
+# creating x ticks
+x_ticks = []
+px = []
+for i in range(int(len(test)/48)):
+    px.append(48*i+48)
+    x_ticks.append('00:00')
 plt.figure(1)
-plt.plot(test,m)
-plt.plot(test,u)
-plt.plot(test,l)
+plt.plot(test,m,label='Predicted')
+plt.plot(x_test,y_test,label='Observed')
+plt.legend()
+plt.ylabel('Power (kW)')
+plt.xlabel('Time')
+plt.xticks(px,x_ticks)
+plt.xlim(0,len(test))
+plt.grid()
+
+#plt.plot(test,u)
+#plt.plot(test,l)
 plt.scatter(np.squeeze(np.asarray(x_train)),np.squeeze(np.asarray(y_train)))
 plt.show()
 
