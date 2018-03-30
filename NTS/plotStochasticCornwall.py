88965e3b979ca1c8c5d49b0f0c6b99e27e0305c8
Constance <constancellc@gmail.com>
2018-03-30 16:10:39 +0100

    no message

--- NTS/plotStochasticCornwall.py
+++ NTS/plotStochasticCornwall.py
@@ -3,29 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-resultsStem = '../../Documents/simulation_results/NTS/national_stochastic/'
+resultsStem = '../../Documents/simulation_results/NTS/cornwall/'
 
-resultsStem2 = '../../Documents/simulation_results/NTS/national/wed/'
 
 plt.figure(1)
 plt.rcParams["font.family"] = 'serif'
 plt.rcParams["font.size"] = 8
 
-t = np.arange(0,64*60)
-plotMonths = {'1':1,'4':2,'7':3,'10':4}
-for month in ['1','4','7','10']:
-    dumb3 = []
-    dumb7 = []
-    with open(resultsStem2+month+'.csv','rU') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)
-        for row in reader:
-            dumb3.append(float(row[2]))
-            dumb7.append(float(row[3]))
-            
-    plt.subplot(2,2,plotMonths[month])
-    plt.plot(t,dumb3,color='#5bcc92',label='Uncontrolled')
-    #plt.plot(t,dumb7,'y',ls='--',label='Uncontrolled Charging 3kW')
     
 months = {'1':'January','4':'April','7':'July','10':'October'}
 n = 1
@@ -36,6 +20,7 @@ for m in months:
 
     t = []
     base = []
+    dumb = []
     p1 = []
     p2 = []
     p3 = []
@@ -43,17 +28,18 @@ for m in months:
     p5 = []
     pav = []
 
-    with open(resultsStem+m+'_experienced.csv','rU') as csvfile:
+    with open(resultsStem+m+'.csv','rU') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)
         for row in reader:
             t.append(float(row[0]))
-            base.append(float(row[1]))
-            p1.append(float(row[2]))
-            p2.append(float(row[3]))
-            p3.append(float(row[4]))
-            p4.append(float(row[5]))
-            p5.append(float(row[6]))
+            dumb.append(float(row[1])+float(row[2]))
+            base.append(float(row[2]))
+            p1.append(float(row[3]))
+            p2.append(float(row[4]))
+            p3.append(float(row[5]))
+            p4.append(float(row[6]))
+            p5.append(float(row[7]))
             pav.append(0.05*p1[-1]+0.2*p2[-1]+0.5*p3[-1]+0.2*p4[-1]+0.05*p5[-1])
 
     min1 = []
@@ -62,7 +48,7 @@ for m in months:
     max2 = []
 
     for i in range(len(base)):
-        lowest = 100
+        lowest = 1000
         highest = 0
         for p in [p2,p3,p4]:
             if p[i] < lowest:
@@ -83,8 +69,8 @@ for m in months:
         max2.append(highest)
     #plt.subplot(1,2,1)
     plt.plot(t,base,'k',ls=':',label='Base load')
+    plt.plot(t,dumb,color='#5bcc92',label='Uncontrolled')
     plt.plot(t,pav,color='#0d50bc',label='Controlled')
-    #plt.fill_between(t,min1,max1,color='b',alpha=0.1)
     plt.fill_between(t,min2,max2,color='#82abed')
 
     x = np.linspace(26*60,46*60,num=6)
@@ -92,16 +78,16 @@ for m in months:
     plt.xticks(x,x_ticks)
     plt.xlim(24*60,48*60)
     plt.grid()
-    #plt.ylim(20,70)
+    plt.ylim(-20,600)
 
     if n == 2 or n == 4:
         plt.ylabel('Power Demand (GW)')
 
-    if n == 5:
+    if n == 2:
         plt.legend(loc=8)
 
 
 
 plt.tight_layout()
-plt.savefig('../../Dropbox/papers/smart-charging/national.eps', format='eps', dpi=1000)
+plt.savefig('../../Dropbox/papers/smart-charging/cornwall.eps', format='eps', dpi=1000)
 plt.show()
